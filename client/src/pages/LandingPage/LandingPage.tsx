f5245c9f86ffa069d3d7bba9bd194a6526c2d188
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-08-08 00:07:29 +0530

    redux toolkit added

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,11 @@
+import { Button } from "../../components/ui/button";
+
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <Button>LandingPage</Button>
+    </div>
+  );
 }
 
 export default LandingPage;
