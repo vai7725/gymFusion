b70ce2341c11ee1f257b76973d899fdfd6795fbb
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-08-07 23:39:46 +0530

    tailwind css added , react router dom added

---
+++ client/src/Layout/HomeLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+const HomeLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default HomeLayout;
--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 function Footer() {
-  return <div>Footer</div>;
+  return <div className="bg-black h-screen">Footer</div>;
 }
 
 export default Footer;
